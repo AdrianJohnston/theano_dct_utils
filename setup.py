b9c04c990b14a2b7155f28c78f4a6b6d9281c1ff
adrian <Adrian.JohnstonAUS@gmail.com>
2016-11-03 15:08:48 +1030

    Finalised setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='theano_dct_utils',
     version='0.1dev',
-    packages=['theano_dct_utils',],
+    py_modules=['theano_dct_utils',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
