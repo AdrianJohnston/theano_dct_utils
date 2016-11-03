8b2595ea6fda0b0c11357f17af5844c9486d9dda
adrian <Adrian.JohnstonAUS@gmail.com>
2016-11-03 14:56:44 +1030

    Restructure for build

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='theano_dct_utils',
+    version='0.1dev',
+    packages=['theano_dct_utils',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
